aeccf13bfeaa8a198e9aa9660bf382e043f245c6
Marta Hilgert <marta.hil.1988@gmail.com>
2025-01-20 18:32:05 -0300

    Style CoffeeList

--- src/CoffeesMainPage/CoffeeList/styles.ts
+++ src/CoffeesMainPage/CoffeeList/styles.ts
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
 
 export const CoffeesContainer = styled.div`
+  margin-left: 10rem;
+  margin-right: 10rem;
+  margin-bottom: 2.6166rem;
+
   h1 {
     margin-bottom: 2.125rem;
+    font-family: 'Baloo 2';
+    font-size: 2rem;
+    font-weight: bolder; /* ExtraBold */
   }
 `
 
